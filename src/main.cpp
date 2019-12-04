7f5c9365057827354d714f48e0712a91bbf9c592
8-Lambda-8 <j.wasle111@gmail.com>
2019-12-05 00:19:11 +0100

    implemented MQTT functions

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,6 @@ uint8_t SwitchPins[] = {1,3};
 
 
 #include "wifiPasswd.h"
-const char* ssid;
-const char* password;
 const char* mqtt_server = "MQTT";
 
 WiFiClient espClient;
@@ -52,6 +50,49 @@ void SwitchRelay(uint8_t x,boolean b){
 
 }
 
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  String msg = "";
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    msg += (char)payload[i];
+  }
+  Serial.println();
+
+  String topicStr = String(topic);
+
+  if(topicStr.substring(0,15) == "/LightSwitch/0/"){
+    
+    SwitchRelay(topicStr.charAt(15)-'0',(char)payload[0]=='1');
+
+  }
+  
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(),"test1","test1")) {
+      Serial.println("connected");
+      
+      client.subscribe("/LightSwitch/0/#");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -60,11 +101,13 @@ void setup() {
   pinMode(RelayPins[0], OUTPUT);
   pinMode(RelayPins[1], OUTPUT);
   pinMode(SwitchPins[0], INPUT);
-  pinMode(SwitchPins[1
-], INPUT);
+  pinMode(SwitchPins[1], INPUT);
+  SwitchRelay(0,false);
+  SwitchRelay(1,false);
 
   setup_wifi();
-  
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
 void loop() {
