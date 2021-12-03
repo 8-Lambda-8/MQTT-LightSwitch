ec136b480d9296a0fe6182a804e3a5225a399903
8-Lambda-8 <j.wasle111@gmail.com>
2021-12-03 11:23:57 +0100

    removed wifi manager

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,24 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <PubSubClient.h>
-#include <WiFiManager.h>
 
 uint8_t RelayPins[] = {2, 0};
 uint8_t SwitchPins[] = {1, 3};
 bool LastSwitchState[sizeof(RelayPins)];
 
-//#include "mqttPwd.h"
+#include "mqttPwd.h"
+#include "wifiPasswd.h"
 
-//#include "wifiPasswd.h"
-
-const char* mqtt_user = "LightSwitch";
-const char* mqtt_pwd = "LightSwitch";
-const char* mqtt_server = "PiServer";
+long mill, mqttConnectMillis, wifiConnectMillis;
 
 const String id = "0";
+const String LightSwitchTopic = "/LightSwitch/" + id + '/';
 const String host = "LightSwitch_" + id;
 
 const char* version = __DATE__ " / " __TIME__;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-
-long mill, mqttConnectMillis, wifiConnectMillis;
-
-String LightSwitchTopic = "/LightSwitch/" + id + "/";
 
 void SwitchRelay(uint8_t x, boolean b) {
   Serial.print("switching Relay ");
@@ -41,9 +34,7 @@ void SwitchRelay(uint8_t x, boolean b) {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
+  Serial.printf("\nMessage arrived [%s](L=%d)\n", topic, length);
   String msg = "";
   for (uint16_t i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
@@ -71,6 +62,28 @@ char* str2ch(String command) {
     return p;
   }
   return const_cast<char*>("");
+}
+
+void setup_wifi() {
+  if (WiFi.status() != WL_CONNECTED) {
+    delay(10);
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.setHostname(str2ch(host));
+    WiFi.begin(ssid, password);
+
+    delay(500);
+    randomSeed(micros());
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("");
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+    }
+  }
 }
 
 void reconnect() {
@@ -104,36 +117,36 @@ void reconnect() {
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(74880);
+  Serial.println("begin Setup");
 
   for (uint8_t i = 0; i < sizeof(RelayPins); i++) {
     pinMode(RelayPins[i], OUTPUT);
     pinMode(SwitchPins[i], INPUT);
 
     SwitchRelay(i, false);
-    LastSwitchState[i] = digitalRead(SwitchPins[i]);
   }
 
-  WiFiManager wifiManager;
-  WiFi.hostname(host);
-  wifiManager.setAPStaticIPConfig(IPAddress(10, 0, 0, 1),
-                                  IPAddress(10, 0, 0, 1),
-                                  IPAddress(255, 255, 255, 0));
-  wifiManager.autoConnect(str2ch(host));
-
+  setup_wifi();  
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 
-  MDNS.begin(host);
-  MDNS.addService("http", "tcp", 80);
-
+  ArduinoOTA.setHostname(str2ch(host));
   ArduinoOTA.begin();
+
+  reconnect();
+  Serial.println("end Setup");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
   ArduinoOTA.handle();
+
+  if ((millis() - wifiConnectMillis) > 10000) {
+    setup_wifi();
+    wifiConnectMillis = millis();
+  }
 
   if ((millis() - mqttConnectMillis) > 5000) {
     reconnect();
